3cf44d08d791f1936f32972b8e13535a386ea916
Hisanori Shimakawa <shimakawa.hisanori@gmail.com>
2023-09-19 10:03:55 +0900

    ログイン画面を追加

---
+++ travel/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+app_name = "accounts"
+
+urlpatterns = [
+    path("", views.IndexView.as_view(), name="index"),
+    path('signup/', views.SignupView.as_view(), name="signup"),
+    path("test/", views.TestView.as_view(), name="test"),
+]
